00eb6fe39450a9acd4632e82bcf7a22e9b47318e
Ibrahim <ibrahim1234ol12@gmail.com>
2022-03-08 23:09:02 +0100

    Adding Pets and contactInfo to pets on bootstrap. Closes #33.

--- pet-clinic-web/src/main/java/com/springboot/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springboot/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.springboot.petclinic.bootstrap;
 
 import com.springboot.petclinic.model.Owner;
+import com.springboot.petclinic.model.Pet;
 import com.springboot.petclinic.model.PetType;
 import com.springboot.petclinic.model.Vet;
 import com.springboot.petclinic.service.OwnerService;
@@ -8,6 +9,8 @@ import com.springboot.petclinic.service.PetTypeService;
 import com.springboot.petclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,14 +39,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Alex");
         owner1.setLastName("Dubi");
+        owner1.setAddress("address1");
+        owner1.setCity("city1");
+        owner1.setTelephone("telephone1");
+
+        Pet owner1Pet = new Pet();
+        owner1Pet.setPetType(dogPetType);
+        owner1Pet.setName("Rosco");
+        owner1Pet.setBirthDate(LocalDate.now());
+        owner1Pet.setOwner(owner1);
+        owner1.getPets().add(owner1Pet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Sam");
         owner2.setLastName("Frank");
-
+        owner2.setAddress("address2");
+        owner2.setCity("city2");
+        owner2.setTelephone("telephone2");
         ownerService.save(owner2);
+
+        Pet owner2Pet = new Pet();
+        owner2Pet.setPetType(catPetType);
+        owner2Pet.setName("sjol");
+        owner2Pet.setBirthDate(LocalDate.now());
+        owner2Pet.setOwner(owner2);
+        owner2.getPets().add(owner2Pet);
 
         System.out.println("Loaded owners.....");
 
