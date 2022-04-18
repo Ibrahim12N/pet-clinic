1ba6b6e701acb3f7c094076cdd8df285052d621c
Ibrahim <ibrahim1234ol12@gmail.com>
2022-04-18 23:09:32 +0200

    adding spring data jpa service for Speciality. Closes #44.

--- pet-clinic-data/src/main/java/com/springboot/petclinic/service/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/service/map/PetMapService.java
@@ -1,14 +1,13 @@
 package com.springboot.petclinic.service.map;
 
 import com.springboot.petclinic.model.Pet;
-import com.springboot.petclinic.service.CrudService;
 import com.springboot.petclinic.service.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
 
     @Override
